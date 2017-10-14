6828b2f7354d5aa22f8978b7ab7c8a4d1b08dea5
Alexander Danilyak <adanilyak@gmail.com>
2017-10-14 11:05:37 +0200

    iOS Init Project

---
+++ iOS/TravelHelper/TravelHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelHelper
+//
+//  Created by Alexander Danilyak on 14/10/2017.
+//  Copyright © 2017 Alexander Danilyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
