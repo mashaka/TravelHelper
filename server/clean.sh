d129c3156164199b7c19c1fbc050002191fe7e85
Mark Riabov <m.ryadom@gmail.com>
2017-10-14 18:13:08 +0200

    Server main part

---
+++ server/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -f app/migrations/0001_initial.py
+rm -f db.sqlite3
+python3 ./manage.py makemigrations
+python3 ./manage.py migrate
+python3 ./manage.py createcachetable
+#python3 ./manage.py loaddata fixtures/init.json
+#/manage.py dumpdata app auth.user social_django --format=json > fixtures/init.json --indent=4
+echo "from django.contrib.auth.models import User; User.objects.filter(username='ryadom').delete(); User.objects.create_superuser('ryadom', '', 'qweqweqwe')" | python manage.py shell
