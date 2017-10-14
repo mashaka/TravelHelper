2eb1e7a9eab3354c56cdb2acbb4763cf172b26b0
Alexander Danilyak <adanilyak@gmail.com>
2017-10-14 11:34:26 +0200

    iOS Libs Update

---
+++ iOS/TravelHelper/TravelHelper/Controllers/LoginVC.swift
@@ -0,0 +1,15 @@
+//
+//  LoginVC.swift
+//  TravelHelper
+//
+//  Created by Alexander Danilyak on 14/10/2017.
+//  Copyright © 2017 Alexander Danilyak. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    
+}
